ed39f2f25df264bd680c4391da523f32a75029c5
million <74927604+Million-art@users.noreply.github.com>
2025-01-03 09:24:05 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
-// Correctly typing __filename and __dirname in TypeScript
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Export the Vite configuration using TypeScript
 export default defineConfig({
   plugins: [react()],
+  base: '/mini_app', 
   build: {
-    outDir: "dist", // Ensure this is 'dist' to match Netlify's publish folder
+    outDir: "dist",
   },
   resolve: {
     alias: {
