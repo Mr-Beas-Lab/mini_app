9d7cf9de311616d9db6063cc671daa57b9ab7c9d
million-art <millionmulugeta09@gmail.com>
2025-01-10 22:59:20 +0300

    referral fix

--- src/components/ReferredUsers.tsx
+++ src/components/ReferredUsers.tsx
@@ -1,34 +1,42 @@
-import { FaLink, FaTelegramPlane, FaShareAlt } from 'react-icons/fa';
+import { FaLink, FaTelegramPlane, FaShareAlt } from "react-icons/fa";
 import { useState, useEffect } from "react";
 import { telegramId } from "@/libs/telegram";
 import { db } from "@/firebase";
-import { collection, query, where, getDocs } from "firebase/firestore";  
+import { collection, query, where, getDocs } from "firebase/firestore";
 import { firstName } from "@/libs/telegram";
 
 const ReferredUsers = () => {
-  const [referrals, setReferrals] = useState<User[]>([]);  
+  const [referrals, setReferrals] = useState<User[]>([]);
   const [isCopied, setIsCopied] = useState(false);
+  const [status, setStatus] = useState<"loading" | "success" | "error">(
+    "loading"
+  );
   const id = String(telegramId);
   const invitationLink = `https://t.me/john_sart_bot?start=ref_${id}`;
 
-  // Fetch referred users from Firestore
   useEffect(() => {
     const fetchReferrals = async () => {
+      setStatus("loading"); // Set loading state
       try {
         const q = query(collection(db, "users"), where("referredBy", "==", id));
         const querySnapshot = await getDocs(q);
-  
-        const referredUsers: User[] = [];
-        querySnapshot.forEach((doc) => {
-          referredUsers.push({ ...doc.data(), id: doc.id } as User);  
-        });
-  
-        setReferrals(referredUsers);
+
+        if (querySnapshot.empty) {
+          setReferrals([]);
+        } else {
+          const referredUsers: User[] = [];
+          querySnapshot.forEach((doc) => {
+            referredUsers.push({ ...doc.data(), id: doc.id } as User);
+          });
+          setReferrals(referredUsers);
+        }
+        setStatus("success"); // Set success state
       } catch (error) {
         console.error("Error fetching referrals: ", error);
+        setStatus("error"); // Set error state
       }
     };
-  
+
     fetchReferrals();
   }, []);
 
@@ -49,15 +57,17 @@ const ReferredUsers = () => {
         </p>
       </div>
 
-      {/* Action buttons */}
       <div className="flex justify-evenly items-center mb-8">
         <div className="text-center flex flex-col">
-          <button className="bg-gray-medium hover:bg-gray-dark rounded p-3 flex items-center justify-center transition duration-300"
+          <button
+            className="bg-gray-medium hover:bg-gray-dark rounded p-3 flex items-center justify-center transition duration-300"
             onClick={copyToClipboard}
           >
             <FaLink className="text-white text-xl" />
           </button>
-          <h2 className="mt-2 text-sm text-white">{isCopied ? 'Copied!' : 'Copy'}</h2>
+          <h2 className="mt-2 text-sm text-white">
+            {isCopied ? "Copied!" : "Copy"}
+          </h2>
         </div>
 
         <div className="text-center flex flex-col">
@@ -65,7 +75,9 @@ const ReferredUsers = () => {
             className="bg-gray-medium text-white py-2 px-4 rounded-lg flex items-center justify-center"
             onClick={() => {
               window.open(
-                `https://t.me/share/url?url=${encodeURIComponent(invitationLink)}&text=${encodeURIComponent(
+                `https://t.me/share/url?url=${encodeURIComponent(
+                  invitationLink
+                )}&text=${encodeURIComponent(
                   `🎁🎁🎁Hello! ${firstName} invited You to earn rewards.🎁🎁🎁 Click the link and Earn :`
                 )}`,
                 "_blank"
@@ -78,72 +90,92 @@ const ReferredUsers = () => {
         </div>
 
         <div className="text-center flex flex-col">
-        <button
-          className="bg-gray-medium hover:bg-gray-dark rounded p-3 flex items-center justify-center transition duration-300"
-          onClick={() => {
-            const shareData = {
-              title: "Earn Rewards!",
-              text: `🎁🎁🎁 Hello! ${firstName} invited You to earn rewards. 🎁🎁🎁 Click the link and Earn:`,
-              url: invitationLink,
-            };
+          <button
+            className="bg-gray-medium hover:bg-gray-dark rounded p-3 flex items-center justify-center transition duration-300"
+            onClick={() => {
+              const shareData = {
+                title: "Earn Rewards!",
+                text: `🎁🎁🎁 Hello! ${firstName} invited You to earn rewards. 🎁🎁🎁 Click the link and Earn:`,
+                url: invitationLink,
+              };
 
-            if (navigator.share) {
-              navigator
-                .share(shareData)
-                .catch((error) => console.error("Error sharing content:", error));
-            } else {
-              // Fallback for unsupported browsers
-              const encodedMessage = encodeURIComponent(
-                `${shareData.text} ${shareData.url}`
-              );
-              const fallbackURL = `https://wa.me/?text=${encodedMessage}`;
-              window.open(fallbackURL, "_blank");
-            }
-          }}
-        >
+              if (navigator.share) {
+                navigator
+                  .share(shareData)
+                  .catch((error) =>
+                    console.error("Error sharing content:", error)
+                  );
+              } else {
+                const encodedMessage = encodeURIComponent(
+                  `${shareData.text} ${shareData.url}`
+                );
+                const fallbackURL = `https://wa.me/?text=${encodedMessage}`;
+                window.open(fallbackURL, "_blank");
+              }
+            }}
+          >
             <FaShareAlt className="text-white text-xl" />
-        </button>
-
+          </button>
           <h2 className="mt-2 text-sm text-white">Share</h2>
         </div>
       </div>
 
-      {/* Referrals Section */}
       <div className="bg-gray-dark rounded-lg p-6 shadow-xl max-h-96 overflow-y-auto">
-  {referrals.length === 0 ? (
-    <div className="text-center text-white">
-      {/* Loading Indicator */}
-      <div className="flex justify-center items-center py-4">
-        <div className="w-12 h-12 border-4 border-t-4 border-gray-600 border-t-transparent rounded-full animate-spin"></div>
-      </div>
-      <p>Loading your referrals...</p>
-    </div>
-  ) : (
-    referrals.map(({ balance, firstName, lastName, userImage }, idx) => (
-      <div key={idx} className="flex items-center justify-between py-4 border-b border-gray-700">
-        <div className="flex items-center">
-          <div className="mr-4 text-white rounded-full w-12 h-12 flex items-center justify-center">
-            <p>{idx + 1}</p>
-          </div>
-          <div className="flex items-center">
-            {userImage ? (
-              <img className="w-12 h-12 rounded-full" src={userImage} alt={firstName} />
-            ) : (
-              <div className="bg-gray-700 text-white w-12 h-12 rounded-full flex items-center justify-center">
-                <p>{firstName[0]}</p>
-              </div>
-            )}
-            <div className="ml-4">
-              <p className="font-semibold text-white">{firstName} {lastName}</p>
-              <p className="text-sm text-gray-400">${balance}</p>
+        {status === "loading" && (
+          <div className="text-center text-white">
+            <div className="flex justify-center items-center py-4">
+              <div className="w-12 h-12 border-4 border-t-4 border-gray-600 border-t-transparent rounded-full animate-spin"></div>
             </div>
+            <p>Loading your referrals...</p>
           </div>
-        </div>
-      </div>
-    ))
-  )}
-    </div>
+        )}
 
+        {status === "error" && (
+          <div className="text-center text-red-500">
+            <p>Error fetching referrals. Please try again later.</p>
+          </div>
+        )}
+
+        {status === "success" && (
+          <>
+            {referrals.length === 0 ? (
+              <p className="text-center text-white">No referrals yet.</p>
+            ) : (
+              referrals.map(({ balance, firstName, lastName, userImage }, idx) => (
+                <div
+                  key={idx}
+                  className="flex items-center justify-between py-4 border-b border-gray-700"
+                >
+                  <div className="flex items-center">
+                    <div className="mr-4 text-white rounded-full w-12 h-12 flex items-center justify-center">
+                      <p>{idx + 1}</p>
+                    </div>
+                    <div className="flex items-center">
+                      {userImage ? (
+                        <img
+                          className="w-12 h-12 rounded-full"
+                          src={userImage}
+                          alt={firstName}
+                        />
+                      ) : (
+                        <div className="bg-gray-700 text-white w-12 h-12 rounded-full flex items-center justify-center">
+                          <p>{firstName[0]}</p>
+                        </div>
+                      )}
+                      <div className="ml-4">
+                        <p className="font-semibold text-white">
+                          {firstName} {lastName}
+                        </p>
+                        <p className="text-sm text-gray-400">${balance}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
