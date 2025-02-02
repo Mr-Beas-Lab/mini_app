cd5fa087ef0ae1285e89c2fd103f4e7e56beaa49
million-art <millionmulugeta09@gmail.com>
2025-02-02 17:51:46 +0300

    tab

--- src/screens/Referrals.tsx
+++ src/screens/Referrals.tsx
@@ -11,8 +11,17 @@ const Referrals = () => {
     <section className="mb-24">
       <Profile />
       <div className="min-h-screen bg-zinc-950 text-white p-6 flex flex-col items-center">
-        <Tabs defaultValue="referral" className="w-full max-w-lg">
+        <Tabs defaultValue="leaderboard" className="w-full max-w-lg">
           <TabsList className="flex w-full bg-zinc-900 p-1 rounded-lg shadow-md">
+          <TabsTrigger
+              value="leaderboard"
+              className="w-1/2 py-2 text-center text-lg font-medium rounded-md transition-all duration-300 
+                         hover:bg-zinc-800 focus:bg-zinc-700 data-[state=active]:bg-zinc-600"
+            >
+              {t("leaderboard.title")}
+
+            </TabsTrigger>
+
             <TabsTrigger
               value="referral"
               className="w-1/2 py-2 text-center text-lg font-medium rounded-md transition-all duration-300 
@@ -22,14 +31,7 @@ const Referrals = () => {
               {t("referral.subtitle")}
 
             </TabsTrigger>
-            <TabsTrigger
-              value="leaderboard"
-              className="w-1/2 py-2 text-center text-lg font-medium rounded-md transition-all duration-300 
-                         hover:bg-zinc-800 focus:bg-zinc-700 data-[state=active]:bg-zinc-600"
-            >
-              {t("leaderboard.title")}
 
-            </TabsTrigger>
           </TabsList>
           <div className="mt-6 p-4 bg-zinc-900 rounded-lg shadow-lg w-full">
             <TabsContent value="referral">
